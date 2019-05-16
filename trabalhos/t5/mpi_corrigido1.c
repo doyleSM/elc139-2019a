1040af036d9b152210c1cd7a5ce3cd7a17a1643c
Gabriel Doyle Balk <gbalk@inf.ufsm.br>
2019-05-16 11:08:36 -0300

    fix

--- trabalhos/t5/mpi_corrigido1.c
+++ trabalhos/t5/mpi_corrigido1.c
@@ -11,36 +11,36 @@
 
 int main(int argc, char *argv[])
 {
-   int numtasks, rank, dest, tag, source, rc;
-   int inmsg, outmsg = 100;
-   MPI_Status stat;
+  int numtasks, rank, dest, tag, source, rc;
+  int inmsg, outmsg = 100;
+  MPI_Status stat;
 
-   MPI_Init(&argc, &argv);
-   MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-   printf("Processo %d iniciando...\n", rank);
+  printf("Processo %d iniciando...\n", rank);
 
-   if (rank == 0)
-   {
-      dest = 1;
-      source = dest;
-      tag = rank;
-      rc = MPI_Send(&outmsg, 1, MPI_INT, dest, tag, MPI_COMM_WORLD);
-      printf("Enviei mensagem para processo %d...\n", dest);
-      rc = MPI_Recv(&inmsg, 1, MPI_INT, source, dest, MPI_COMM_WORLD, &stat);
-      printf("Recebi mensagem do processo %d...\n", source);
-   }
-   else if (rank == 1)
-   {
-      dest = 0;
-      source = dest;
-      tag = rank;
-      rc = MPI_Recv(&inmsg, 1, MPI_INT, source, dest, MPI_COMM_WORLD, &stat);
-      printf("Recebi mensagem do processo %d...\n", source);
-      rc = MPI_Send(&outmsg, 1, MPI_INT, dest, tag, MPI_COMM_WORLD);
-      printf("Enviei mensagem para processo %d...\n", dest);
-   }
+  if (rank == 0)
+  {
+    dest = 1;
+    source = dest;
+    tag = rank;
+    rc = MPI_Send(&outmsg, 1, MPI_INT, dest, tag, MPI_COMM_WORLD);
+    printf("Enviei mensagem para processo %d...\n", dest);
+    rc = MPI_Recv(&inmsg, 1, MPI_INT, source, dest, MPI_COMM_WORLD, &stat);
+    printf("Recebi mensagem do processo %d...\n", source);
+  }
+  else if (rank == 1)
+  {
+    dest = 0;
+    source = dest;
+    tag = rank;
+    rc = MPI_Recv(&inmsg, 1, MPI_INT, source, dest, MPI_COMM_WORLD, &stat);
+    printf("Recebi mensagem do processo %d...\n", source);
+    rc = MPI_Send(&outmsg, 1, MPI_INT, dest, tag, MPI_COMM_WORLD);
+    printf("Enviei mensagem para processo %d...\n", dest);
+  }
 
-   MPI_Finalize();
+  MPI_Finalize();
 }
